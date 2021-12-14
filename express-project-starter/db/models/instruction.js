df83b6dbf9b1eff972a30f183b5a81b00c71461d
Ryan Bender <ryan_bender@ymail.com>
2021-12-13 20:03:14 -0600

    database setup

---
+++ express-project-starter/db/models/instruction.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Instruction = sequelize.define('Instruction', {
+    specification: DataTypes.STRING,
+    listOrder: DataTypes.INTEGER,
+    recipeId: DataTypes.INTEGER
+  }, {});
+  Instruction.associate = function(models) {
+    // associations can be defined here
+    Instruction.belongsTo(models.Recipe, {foreignKey: 'recipeId'})
+  };
+  return Instruction;
+};
