0d642a5fc48a17f0c4a472d548077004669bca7b
Kevin Pravia <keserrano00@gmail.com>
2021-12-14 17:00:46 -0700

    add board

--- express-project-starter/routes/boards.js
+++ express-project-starter/routes/boards.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const { check, validationResult } = require('express-validator');
+const { check, validationResult } = require('express-validator');
 const { csrfProtection, asyncHandler } = require('./utils');
 const db = require('../db/models'); //db.Model
 const { requireAuth } = require('../auth');
@@ -11,6 +11,42 @@ router.get('/', requireAuth, asyncHandler(async(req, res) => {
     const {userId} = req.session.auth
     const boards = await db.Board.findAll({where: {userId}});
     res.render('boards', {title: 'Recipeats | Boards', boards})
+}));
+
+let errors = []
+
+router.get('/new', requireAuth, csrfProtection, asyncHandler(async(req, res) => {
+    res.render('new-board', { title: 'Recipeats | New Board', csrfToken: req.csrfToken(), errors });
+}));
+
+const boardValidator = [
+    check('boardName')
+    .exists({checkFalsy: true})
+    .withMessage('Please provide a board name')
+];
+
+router.post('/new', requireAuth, boardValidator, csrfProtection, asyncHandler(async(req, res) => {
+    const name = req.body.boardName;
+    console.log(req.boardName);
+    const userId = req.session.auth.userId
+    let board = db.Board.build({
+        name,
+        userId
+    });
+
+    const validatorErrors = validationResult(req);
+
+    if(validatorErrors.isEmpty()) {
+        await board.save();
+        res.redirect(`/boards`)
+    }
+    else {
+        errors = validatorErrors.array().map((error) => error.msg);
+        res.render('new-board', {
+            title: 'Recipeats | New Board', csrfToken: req.csrfToken(), errors
+        });
+        errors = [];
+    }
 }));
 
 module.exports = router;
